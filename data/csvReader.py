306334382b44209c1262959a41585c9291929481
Jacob Thompson <jacob.s.thompson@icloud.com>
2025-08-13 15:22:02 -0700

    task two

---
+++ data/csvReader.py
@@ -0,0 +1,13 @@
+import pandas
+
+df = pandas.read_csv('daily_sales_data_0.csv', header=None)
+
+productName = 'pink morsel'
+pink_df = df[df[0] == productName].copy()
+
+
+pink_df['sales'] = pink_df[1].replace('[\$,]', '', regex=True).astype(float) * pink_df[2].astype(float)
+
+final_df = pink_df[['sales',3,4]]
+final_df.columns = ['Sales','Date','Region']
+final_df.to_csv('sales_data.csv', index=False)
