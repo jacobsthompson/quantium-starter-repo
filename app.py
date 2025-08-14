5ddf72d3ced3599e295df2af2aeddc395645292f
Jacob Thompson <jacob.s.thompson@icloud.com>
2025-08-13 17:02:13 -0700

    task four

--- app.py
+++ app.py
@@ -9,29 +9,41 @@ import pandas as pd
 df = pd.read_csv('sales_data.csv')
 df.head()
 
-app = Dash()
+app = Dash(__name__)
 
 regions = df['Region'].unique()
 
-color_map = {
-    'north': '#1f77b4',
-    'south': '#ff7f0e',
-    'east': '#2ca02c',
-    'west': '#d62728',
+colors = {
+    'background': '#111111',
+    'text': '#7FDBFF'
 }
 
-app.layout = html.Div(children=[
+color_map = {
+    'north': '#D9415D',
+    'south': '#856DA6',
+    'east': '#4EA67D',
+    'west': '#F2AC57',
+}
+
+app.layout = html.Div(style={
+    'backgroundColor': colors['background'],
+    'fontFamily': 'Arial, sans-serif',
+    'textAlign': 'center',
+    'color': colors['text']
+},children=[
     html.H1(children='Pink Morsel Sales'),
 
     html.Div(children='''
         A Line Chart Visualization of Pink Morsel Sales Over Time in Various Regions.
     '''),
 
-    dcc.Checklist(
+    dcc.RadioItems(
         id='region-selection',
-        options=[{'label': r, 'value': r} for r in regions],
-        value=list(regions),
-        inline=True
+        options=[{'label': 'All', 'value': 'All'}] +
+                [{'label': r, 'value': r} for r in regions],
+        value='All',
+        inline=True,
+        style={'margin': '20px'}
     ),
 
     dcc.Graph(
@@ -44,8 +56,26 @@ app.layout = html.Div(children=[
               )
 
 def update_chart(selected_region):
-    filtered_df = df[df['Region'].isin(selected_region)]
-    fig = px.line(filtered_df, x="Date", y="Sales", color="Region", color_discrete_map=color_map)
+    if selected_region == 'All':
+        filtered_df = df
+    else:
+        filtered_df = df[df['Region'] == selected_region]
+
+    fig = px.line(
+        filtered_df,
+        x="Date",
+        y="Sales",
+        color="Region",
+        color_discrete_map=color_map,
+        template='seaborn',
+        )
+
+    fig.update_traces(line=dict(width=1))
+    fig.update_layout(
+        plot_bgcolor=colors['background'],
+        paper_bgcolor=colors['background'],
+        font_color=colors['text']
+    )
     return fig
 
 if __name__ == '__main__':
